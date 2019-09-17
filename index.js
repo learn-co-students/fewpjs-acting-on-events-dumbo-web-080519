9b960627fcf4cc5a3dda30361c7f83676baae866
Jmendoza173 <Joelmendza173@gmail.com>
2019-09-17 10:48:38 -0400

    Done.

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function moveDodgerLeft() {
   }
 
   function moveDodgerRight() {
-    let rightNumbers = dodger.style.right.replace("px", "");
+    let rightNumbers = dodger.style.left.replace("px", "");
     let right = parseInt(rightNumbers, 10);
   
-    if (right > 0) {
-      dodger.style.right = `${right - 1}px`;
+    if (right <360) {
+      dodger.style.left = `${right + 1}px`;
 
     }
   }
